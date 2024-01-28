2fcd02434257d9d734d5c29567555132843280cd
david <david@DAVID.home>
2024-01-28 15:28:38 +0100

    COMMIT

--- POGRAMACION_EJERCICIOS_DAVID/src/POO4/SistemadeCalificaciones.java
+++ POGRAMACION_EJERCICIOS_DAVID/src/POO4/SistemadeCalificaciones.java
@@ -1,33 +1,37 @@
 package POO4;
 
 public class SistemadeCalificaciones {
-	private String nombrecurso;
-	private double calificacion;
-	public SistemadeCalificaciones () {
-	}
-
-
-	public String getNombrecurso() {
-		return nombrecurso;
-	}
-
-	public void setNombrecurso(String nombrecurso) {
-		this.nombrecurso = nombrecurso;
-	}
-
-	public double getCalificacion() {
-		return calificacion;
-	}
-
-	public void setCalificacion(double calificacion) {
-		this.calificacion = calificacion;
-	}
-
-	public void mostrarinfo() {
-		System.out.println(" el nombre del curso es " + this.getNombrecurso());
-		System.out.println("La calificacion es " + this.getCalificacion() );
-		
-
-	}
-	}
-
+	private String nombreCurso;
+    private double calificacion;
+    public String getNombreCurso() {
+        return nombreCurso;
+    }
+    public void setNombreCurso(String nombreCurso) {
+        this.nombreCurso = nombreCurso;
+    }
+ 
+    public double getCalificacion() {
+        return calificacion;
+    }
+ 
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+ 
+    public String categorizarCalificacion() {
+    	 int calificacionRedondeada = (int) Math.round(calificacion);
+         switch (calificacionRedondeada) {
+             case 10:
+             case 9:
+                 return "A";
+             case 8:
+                 return "B";
+             case 7:
+                 return "C";
+             case 6:
+                 return "D";
+             default:
+                 return "F";
+         }
+     }
+}
--- POGRAMACION_EJERCICIOS_DAVID/src/POO4/SistemadeCalificaciones_main.java
+++ POGRAMACION_EJERCICIOS_DAVID/src/POO4/SistemadeCalificaciones_main.java
@@ -5,33 +5,17 @@ import javax.swing.JOptionPane;
 public class SistemadeCalificaciones_main {
 
 	public static void main(String[] args) {
-		SistemadeCalificaciones notas = new SistemadeCalificaciones();
-		
-		String curso = JOptionPane.showInputDialog("Ingresa el curso del estudiante");
-        double calificacion = Double.parseDouble(JOptionPane.showInputDialog("Ingresa la calificación para el curso " + curso));
-      
-        int calificacionRedondeada = (int) Math.round(calificacion);
-      
-        switch (calificacionRedondeada) {
-            case 1:
-                System.out.println("Tu calificación es A, ¡enhorabuena!");
-                break;
-            case 2:
-                System.out.println("Tu calificación es B, ¡sigue así!");
-                break;
-            case 3:
-                System.out.println("Tu calificación es C, no está mal");
-                break;
-            case 4:
-                System.out.println("Tu calificación es D, esfuérzate más");
-                break;
-            case 5:
-                System.out.println("Tu calificación es F, esfuérzate más");
-                break;
-            default:
-                System.out.println("Calificación no válida");
+		SistemadeCalificaciones curso = new SistemadeCalificaciones();
+        String nombreCurso = JOptionPane.showInputDialog(null, "Ingrese el nombre del curso:");
+        curso.setNombreCurso(nombreCurso);
+        String calificacionStr = JOptionPane.showInputDialog(null, "Ingrese la calificación del curso:");
+        try {
+            double calificacion = Double.parseDouble(calificacionStr);
+            curso.setCalificacion(calificacion);
+            String categoria = curso.categorizarCalificacion();
+            JOptionPane.showMessageDialog(null, "Categoría de calificación en " + curso.getNombreCurso() + ": " + categoria);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error: Ingrese una calificación válida.");
         }
-	}
-	}
-
-
+    }
+}
