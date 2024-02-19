e5a2aa6a42c13093778ed1673871f438a1a40148
DaviidDAM1 <davidvaldessilo@gmail.com>
2024-02-19 09:24:00 +0100

    commit

---
+++ POGRAMACION_EJERCICIOS_DAVID/src/POOherencia/Figura.java
@@ -0,0 +1,30 @@
+package POOherencia;
+
+	abstract class Figura {
+		 public abstract double calcularArea();
+		}
+		class Circulo extends Figura {
+		    double radio;
+
+		    public Circulo(double radio) {
+		        this.radio = radio;
+		    }
+		    public double calcularArea() {
+		        return Math.PI * Math.pow(radio, 2);
+		    }
+		}
+
+		class Rectangulo extends Figura {
+		    double base;
+		    double altura;
+
+		    public Rectangulo(double base, double altura) {
+		        this.base = base;
+		        this.altura = altura;
+		    }
+		    public double calcularArea() {
+		        return base * altura;
+		    }
+		}
+
+
---
+++ POGRAMACION_EJERCICIOS_DAVID/src/POOherencia/Figura_main.java
@@ -0,0 +1,18 @@
+package POOherencia;
+
+public class Figura_main {
+
+	public static void main(String[] args) {
+		Figura[] figuras = new Figura[2];
+        figuras[0] = new Circulo(5); 
+        figuras[1] = new Rectangulo(4, 6); 
+
+        for (Figura figura : figuras) {
+            System.out.println("Área de la figura: " + figura.calcularArea());
+        }
+    }
+}
+
+	
+
+
